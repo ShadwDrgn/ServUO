3895fe45b7fcc92365cb6e0546ae3d893ccf2074
ShadwDrgn <c.arcane1@gmail.com>
2024-07-12 19:52:07 -0400

    fix stupid stuff

--- Scripts/Items/Addons/SpinningwheelEastAddon.cs
+++ Scripts/Items/Addons/SpinningwheelEastAddon.cs
@@ -7,7 +7,7 @@ namespace Server.Items
     public interface ISpinningWheel
     {
         bool Spinning { get; }
-        void BeginSpin(SpinCallback callback, Mobile from, int hue, Item m_Wool);
+        void BeginSpin(SpinCallback callback, Mobile from, Items m_Wool);
     }
 
     public class SpinningwheelEastAddon : BaseAddon, ISpinningWheel
@@ -74,7 +74,7 @@ namespace Server.Items
             }
         }
 
-        public void EndSpin(SpinCallback callback, Mobile from, int hue)
+        public void EndSpin(SpinCallback callback, Mobile from, int hue, int stacksize)
         {
             if (m_Timer != null)
                 m_Timer.Stop();
