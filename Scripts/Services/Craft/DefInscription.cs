eef86a9cbda0f39e917f3d5656959e26604174cc
ShadwDrgn <c.arcane1@gmail.com>
2024-07-18 21:25:31 -0400

    fix spellbook craft chance

--- Scripts/Services/Craft/DefInscription.cs
+++ Scripts/Services/Craft/DefInscription.cs
@@ -361,7 +361,7 @@ namespace Server.Engines.Craft
 
             AddCraft(typeof(BulkOrders.BulkOrderBook), 1044294, 1028793, 65.0, 115.0, typeof(BlankScroll), 1044377, 10, 1044378);
 
-            AddCraft(typeof(Spellbook), 1044294, 1023834, 50.0, 126, typeof(BlankScroll), 1044377, 10, 1044378);
+            AddCraft(typeof(Spellbook), 1044294, 1023834, 50.0, 150, typeof(BlankScroll), 1044377, 10, 1044378);
 
             index = AddCraft(typeof(ScrappersCompendium), 1044294, 1072940, 75.0, 125.0, typeof(BlankScroll), 1044377, 100, 1044378);
             AddRes(index, typeof(DreadHornMane), 1032682, 1, 1044253);
